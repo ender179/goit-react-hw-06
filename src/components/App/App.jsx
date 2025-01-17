69df7b94465edc7d5f19531a5bcc2822010e83ae
ender179 <kirilukvlad91@gmail.com>
2025-01-17 16:59:20 +0200

    git push

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,24 +1,24 @@
-import React from 'react';
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
-import { store, persistor } from '../redux/store';
-import ContactsForm from '../components/ContactsForm/ContactsForm';
-import ContactList from '../components/ContactList/ContactList';
-import SearchBox from '../components/SearchBox/SearchBox';
-import styles from './App.module.css';
+import React from 'react';  
+import { Provider } from 'react-redux';  
+import { PersistGate } from 'redux-persist/integration/react';  
+import { store, persistor } from '../redux/store';  
+import ContactsForm from './ContactsForm/ContactsForm'; // Исправленный импорт  
+import ContactList from './ContactList/ContactList';  
+import SearchBox from './SearchBox/SearchBox';  
+import styles from './App.module.css';  
 
-const App = () => (
-  <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
-      <div className={styles.container}>
-        <h1>Phonebook</h1>
-        <ContactsForm />
-        <h2>Contacts</h2>
-        <SearchBox />
-        <ContactList />
-      </div>
-    </PersistGate>
-  </Provider>
-);
+const App = () => (  
+  <Provider store={store}>  
+    <PersistGate loading={null} persistor={persistor}>  
+      <div className={styles.container}>  
+        <h1>Phonebook</h1>  
+        <h2>Contacts</h2>  
+        <ContactsForm />  
+        <SearchBox />  
+        <ContactList />  
+      </div>  
+    </PersistGate>  
+  </Provider>  
+);  
 
 export default App;
--- src/components/ContactsForm/ContactsForm.jsx
+++ src/components/ContactsForm/ContactsForm.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
-import { addContact } from '../../redux/contactsSlice';
+import { addContact } from '../redux/contactsSlice';
 import styles from './ContactsForm.module.css';
 
 const ContactsForm = () => {

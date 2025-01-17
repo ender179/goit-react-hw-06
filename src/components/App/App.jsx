d4957c89625d0e4aa656e4c89ba5de0e2db6d2fb
ender179 <kirilukvlad91@gmail.com>
2025-01-17 17:23:32 +0200

    git puh

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,24 +1,17 @@
 import React from 'react';  
-import { Provider } from 'react-redux';  
-import { PersistGate } from 'redux-persist/integration/react';  
-import { store, persistor } from '../redux/store';  
-import ContactsForm from './ContactsForm/ContactsForm'; // Исправленный импорт  
 import ContactList from './ContactList/ContactList';  
+import ContactsForm from './ContactsForm/ContactsForm';  
 import SearchBox from './SearchBox/SearchBox';  
 import styles from './App.module.css';  
 
 const App = () => (  
-  <Provider store={store}>  
-    <PersistGate loading={null} persistor={persistor}>  
-      <div className={styles.container}>  
-        <h1>Phonebook</h1>  
-        <h2>Contacts</h2>  
-        <ContactsForm />  
-        <SearchBox />  
-        <ContactList />  
-      </div>  
-    </PersistGate>  
-  </Provider>  
+  <div className={styles.container}>  
+    <h1>Phonebook</h1>  
+    <h2>Contacts</h2>  
+    <ContactsForm />  
+    <SearchBox />  
+    <ContactList />  
+  </div>  
 );  
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,17 @@
-import React, { StrictMode } from "react";  
-import { createRoot } from "react-dom/client";  
-import './index.css';  
-import App from "./components/App/App.jsx";  
-import { Provider } from "react-redux";  
-import store from "./redux/store"; // импортируем store  
+import React, { StrictMode } from 'react';  
+import { createRoot } from 'react-dom/client';  
+import App from './components/App/App';  
+import { Provider } from 'react-redux';  
+import { PersistGate } from 'redux-persist/integration/react';  
+import store, { persistor } from './redux/store'; // Імпортуємо store та persistor  
 
 const root = createRoot(document.getElementById("root"));  
 root.render(  
-    <StrictMode>  
-        <Provider store={store}>  
-            <App />  
-        </Provider>  
-    </StrictMode>  
+  <StrictMode>  
+    <Provider store={store}>  
+      <PersistGate loading={null} persistor={persistor}>  
+        <App />  
+      </PersistGate>  
+    </Provider>  
+  </StrictMode>  
 );

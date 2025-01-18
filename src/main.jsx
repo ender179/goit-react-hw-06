0cfdae75d4eb47d65799211531fa4e688ef36086
ender179 <kirilukvlad91@gmail.com>
2025-01-18 17:42:56 +0200

    git push

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,18 @@
-import React from "react";
-import "./index.css";
-import App from "./App.jsx";
-import { Provider } from "react-redux";
-import ReactDOM from "react-dom/client";
-import { store } from "./redux/store.js";
-import { PersistGate } from "redux-persist/integration/react";
+import React from "react";  
+import ReactDOM from "react-dom/client";  
+import "./index.css";  
+import App from "./App.jsx";  
+import { Provider } from "react-redux";  
+import { PersistGate } from "redux-persist/integration/react";  
+import { store, persistor } from "./redux/store.js"; // Переконайтеся, що шлях правильний  
+import Loading from "./components/Loading"; // Додайте компонент завантаження  
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(  
+  <React.StrictMode>  
+    <Provider store={store}>  
+      <PersistGate loading={<Loading />} persistor={persistor}>  
+        <App />  
+      </PersistGate>  
+    </Provider>  
+  </React.StrictMode>  
 );

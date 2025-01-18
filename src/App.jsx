e21a2a8e1c3d036c3f0e4310f511bc621a75925e
ender179 <kirilukvlad91@gmail.com>
2025-01-18 19:29:03 +0200

    git push

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,17 @@
 import React from "react";  
-import { Provider } from "react-redux";  
-import { PersistGate } from "redux-persist/integration/react";  
-import { persistStore } from "redux-persist";  
-import { store } from './redux/store'; 
-import ContactList from "./components/ContactList/ContactList";  
-import SearchBox from "./components/SearchBox/SearchBox";  
 import ContactForm from "./components/ContactForm/ContactForm";  
-
-const persistor = persistStore(store);  
+import SearchBox from "./components/SearchBox/SearchBox";  
+import ContactList from "./components/ContactList/ContactList";  
 
 function App() {  
     return (  
-        <Provider store={store}>  
-            <PersistGate loading={<Loading />} persistor={persistor}>  
-                <div>  
-                    <h1>Phonebook</h1>  
-                    <ContactForm />  
-                    <h2>Contacts</h2>  
-                    <SearchBox />  
-                    <ContactList />  
-                </div>  
-            </PersistGate>  
-        </Provider>  
+        <div>  
+            <h1>Phonebook</h1>  
+            <ContactForm />  
+            <h2>Contacts</h2>  
+            <SearchBox />  
+            <ContactList />  
+        </div>  
     );  
 }  
 

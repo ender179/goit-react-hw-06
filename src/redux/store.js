cf3140737cb045837638a14bac7c879ab1e91d4f
ender179 <kirilukvlad91@gmail.com>
2025-01-18 21:07:19 +0200

    git push

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,43 +1,41 @@
-import { configureStore } from "@reduxjs/toolkit";  
-import {  
-  persistStore,  
-  persistReducer,  
-  FLUSH,  
-  REHYDRATE,  
-  PAUSE,  
-  PERSIST,  
-  PURGE,  
-  REGISTER,  
-} from "redux-persist";  
-import storage from "redux-persist/lib/storage";  
-import contactsReducer from "./contactsSlice";  
-import filtersReducer from "./filtersSlice";  
+import { configureStore } from "@reduxjs/toolkit";
+import contactsReducer from "./contactsSlice";
+import filtersReducer from "./filtersSlice";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
-// Конфигурация для хранения  
-const persistConfig = {  
-  key: "root", // основной ключ для хранения  
-  storage, // используем localStorage  
-};  
 
-// Создаем корневой редьюсер  
-const rootReducer = {  
-  contacts: contactsReducer,  
-  filters: filtersReducer,  
-};  
+const contactsPersistConfig = {
+  key: "contacts",
+  storage,
+};
+const persistedContactsReducer = persistReducer(
+  contactsPersistConfig,
+  contactsReducer
+);
 
-// Создаем персистированный редьюсер  
-const persistedReducer = persistReducer(persistConfig, rootReducer);  
 
-// Настройка хранилища Redux с учетом персистенции  
-export const store = configureStore({  
-  reducer: persistedReducer,  
-  middleware: (getDefaultMiddleware) =>  
-    getDefaultMiddleware({  
-      serializableCheck: {  
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],  
-      },  
-    }),  
-});  
+export const store = configureStore({
+  reducer: {
+    contacts: persistedContactsReducer,
+    filters: filtersReducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
 
-// Создаем и экспортируем персистор  
+
 export const persistor = persistStore(store);
